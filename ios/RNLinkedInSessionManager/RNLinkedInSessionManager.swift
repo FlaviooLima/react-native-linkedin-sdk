7192e907dfddec9e2ec6bf6279179284c00f3827
Flavio Lima <limafr.flavio.work@gmail.com>
2019-02-01 12:31:42 +0100

    Update RNLinkedInSessionManager.swift
    Force to use the browser instead of an app to complete the register/login process.

--- ios/RNLinkedInSessionManager/RNLinkedInSessionManager.swift
+++ ios/RNLinkedInSessionManager/RNLinkedInSessionManager.swift
@@ -29,7 +29,8 @@ class RNLinkedInSessionManager: NSObject {
         state: state,
         permissions: scopes,
         redirectUrl: redirectUri
-      )
+      ),
+      nativeAppChecker: WebLoginOnly()
     )
   }
   
